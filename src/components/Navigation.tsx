76c1af3010117bdc1152646136d7f869627ceec3
Myroslav13 <myroslavshchuruk@gmail.com>
2025-08-23 19:35:43 +0300

    added tablet and mobile adaptation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface Props {
     countItems: number;
@@ -9,82 +9,133 @@ interface Props {
 
 function Navigation({countItems, addToCart, setCountItems, setAddToCart}: Props) {
     const [cartChecking, setCartChecking] = useState(false)
+    const [isSideMenuShowed, setIsSideMenuShowed] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 768) {
+                setIsSideMenuShowed(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
 
     return (
-        <div className='d-flex align-items-center justify-content-between border-bottom border-1'>
-            <nav className="navbar navbar-expand-lg p-0">
-                <div className="container-fluid gap-5 align-items-center">
-                    <img src="images/logo.svg" alt="logo"></img>
-                    
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-            
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav gap-3">
-                            <li className="nav-item">
-                                <a className="nav-link">Collections</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link">Men</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link">Women</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-
-            <div className='d-flex align-items-center justify-content-between gap-4 position-relative'>
-                <div>
-                    <img src='images/icon-cart.svg' className="img-cart position-relative" style={{cursor: "pointer"}} width={20} alt="icon-cart" onClick={() => setCartChecking(prev => !prev)}></img>
-                    <div className="div-items-number position-absolute">{(countItems !== 0) && (addToCart === true) ? countItems : <></>}</div>
-                </div>
-                <img src='images/image-avatar.png' className="img-avatar rounded-circle" width={35} alt="image-avatar"></img>
-
-                {cartChecking === true ?
-                    <div className="position-absolute div-cart bg-white">
-                        <div style={{padding: "20px"}}>
-                            <h6 className="text-black p-0 m-0">Cart</h6>
+        <>
+            <div className='div-nav d-flex align-items-center justify-content-between border-bottom border-1 position-relative'>
+                <nav className="navbar navbar-expand-md p-0">
+                    <div className="container-fluid gap-1 gap-md-5 align-items-center flex-row-reverse flex-md-row py-3 py-md-0">
+                        <img src="images/logo.svg" alt="logo"></img>
+                        
+                        <button className="navbar-toggler border-0" type="button" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setIsSideMenuShowed(true)}>
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav gap-3">
+                                <li className="nav-item">
+                                    <a className="nav-link">Collections</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link">Men</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link">Women</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link">About</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link">Contact</a>
+                                </li>
+                            </ul>
                         </div>
+                    </div>
+                </nav>
 
-                        <hr className="m-0"></hr>
+                <div className='d-flex align-items-center justify-content-between gap-4 position-static position-md-relative'>
+                    <div className="position-relative">
+                        <img src='images/icon-cart.svg' className="img-cart" style={{cursor: "pointer"}} width={20} alt="icon-cart" onClick={() => setCartChecking(prev => !prev)}></img>
+                        <div className="div-items-number position-absolute">{(countItems !== 0) && (addToCart === true) ? countItems : <></>}</div>
+                    </div>
+                    <img src='images/image-avatar.png' className="img-avatar rounded-circle" width={35} alt="image-avatar" title="You"></img>
 
-                        <div style={{padding: "20px"}}>
-                            {(countItems !== 0 && addToCart === true) ?
-                                <>
-                                    <div className="d-flex align-items-center justify-content-between">
-                                        <img src="images/image-product-1-thumbnail.jpg" className="rounded-3" width={50} height={50} alt="image-product-1-thumbnail"></img>
-                                        
-                                        <div>
-                                            <p className="m-0">Fall Limited Edition Sneakers</p>
-                                            <p className="m-0">$125.00 x {countItems} <b className="text-black">${125 * countItems}.00</b></p>
+                    {cartChecking === true ?
+                        <div className="position-absolute div-cart bg-white" style={{zIndex: "9999"}}>
+                            <div style={{padding: "20px"}}>
+                                <h6 className="text-black p-0 m-0">Cart</h6>
+                            </div>
+
+                            <hr className="m-0"></hr>
+
+                            <div style={{padding: "20px"}}>
+                                {(countItems !== 0 && addToCart === true) ?
+                                    <>
+                                        <div className="d-flex align-items-center justify-content-between">
+                                            <img src="images/image-product-1-thumbnail.jpg" className="rounded-3" width={50} height={50} alt="image-product-1-thumbnail"></img>
+                                            
+                                            <div>
+                                                <p className="m-0">Fall Limited Edition Sneakers</p>
+                                                <p className="m-0">$125.00 x {countItems} <b className="text-black">${125 * countItems}.00</b></p>
+                                            </div>
+
+                                            <img src="images/icon-delete.svg" style={{cursor: "pointer"}} alt="icon-delete" onClick={() => {setCountItems(0); setAddToCart(false)}}></img>
                                         </div>
 
-                                        <img src="images/icon-delete.svg" style={{cursor: "pointer"}} alt="icon-delete" onClick={() => {setCountItems(0); setAddToCart(false)}}></img>
-                                    </div>
+                                        <button className="btn-checkout w-100" style={{marginTop: "20px"}}>Checkout</button>
+                                    </>
+                                    :
+                                    <>
+                                        <p className="text-center py-5 m-0"><b>Your cart is empty.</b></p>
+                                    </>
+                                }
+                            </div>
+                        </div>
+                    :
+                        <></>
+                    }
+                </div>
+            </div>
 
-                                    <button className="btn-checkout w-100" style={{marginTop: "20px"}}>Checkout</button>
-                                </>
-                                :
-                                <>
-                                    <p className="text-center py-5 m-0"><b>Your cart is empty.</b></p>
-                                </>
-                            }
+            {(isSideMenuShowed === true) ? 
+                <>
+                    <div className="modal-backdrop fade show"></div>
+                    
+                    <div className="div-modal modal show d-block bg-white" tabIndex={-1}>
+                        <div className="modal-dialog ms-3 mt-2">
+                            <div className="modal-content" style={{backgroundColor: "transparent", border: "none"}}>
+                                <div className="modal-header pt-1 px-0 d-flex justify-content-start" style={{border: "none"}}>
+                                    <button type="button" className="btn-close m-0 ps-3" data-bs-dismiss="modal" aria-label="Close" onClick={() => setIsSideMenuShowed(false)}></button>
+                                </div>
+                                <div>
+                                    <ul className="navbar-nav gap-1 ps-3">
+                                        <li className="nav-item">
+                                            <a className="nav-link">Collections</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link fs-bold">Men</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link">Women</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link">About</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                :
-                    <></>
-                }
-            </div>
-        </div>
+                </>
+                : 
+                <></>
+            }
+        </>
     )
 }
 
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -13,11 +13,15 @@ function Product({countItems, setCountItems, setAddToCart}: Props) {
 
     return (
         <>
-            <div className="row align-items-start mt-0 mt-lg-4 px-0 px-lg-5">
-                <div className="col-12 col-lg-5">
-                    <img src={`images/image-product-${activePhoto + 1}.jpg`} className='img-fluid rounded-0 rounded-lg-4' style={{width: "100%"}} alt={`image-product-${activePhoto + 1}`} onClick={() => {setIsModalShowed(true); setActiveModalPhoto(activePhoto)}}></img>
+            <div className="row align-items-start m-0 pt-0 pt-md-4 px-0 px-md-5">
+                <div className="col-12 col-md-5 p-0 p-md-auto">
+                    <div className='d-flex align-items-center'>
+                        <button className="d-block d-md-none btn-modal-prev rounded-circle position-absolute" onClick={() => setActivePhoto(prev => prev === 0 ? 3 : prev - 1)}><img src="images/icon-previous.svg"></img></button>
+                        <img src={`images/image-product-${activePhoto + 1}.jpg`} className='img-fluid img-product' alt={`image-product-${activePhoto + 1}`} onClick={() => {setIsModalShowed(true); setActiveModalPhoto(activePhoto)}}></img>
+                        <button className="d-block d-md-none btn-modal-next rounded-circle position-absolute" onClick={() => setActivePhoto(prev => prev === 3 ? 0 : prev + 1)}><img src="images/icon-next.svg"></img></button>
+                    </div>
 
-                    <div className='d-none d-lg-flex align-items-center justify-content-between mt-4'>
+                    <div className='d-none d-md-flex align-items-center justify-content-between mt-4 border-none'>
                         <div className={`img-thumb ${activePhoto === 0 ? "active": ""}`}><img src="images/image-product-1-thumbnail.jpg" className="img-fluid" onClick={() => setActivePhoto(0)} alt="image-product-1-thumbnail"></img></div>
                         <div className={`img-thumb ${activePhoto === 1 ? "active": ""}`}><img src="images/image-product-2-thumbnail.jpg" className="img-fluid" onClick={() => setActivePhoto(1)} alt="image-product-2-thumbnail"></img></div>
                         <div className={`img-thumb ${activePhoto === 2 ? "active": ""}`}><img src="images/image-product-3-thumbnail.jpg" className="img-fluid" onClick={() => setActivePhoto(2)} alt="image-product-3-thumbnail"></img></div>
@@ -25,30 +29,30 @@ function Product({countItems, setCountItems, setAddToCart}: Props) {
                     </div>
                 </div>
 
-                <div className="div-product-data col-12 col-lg-7 d-flex flex-column align-items-start my-auto px-5 px-lg-auto">
+                <div className="div-product-data col-12 col-md-7 d-flex flex-column align-items-start my-auto px-5 px-md-auto py-4 py-md-auto">
                     <h4 className='text-uppercase' style={{fontSize: "12px", letterSpacing: "2px"}}>Sneaker Company</h4>
 
                     <h1 className='fw-bold fs-1'>Fall Limited Edition Sneakers</h1>
 
-                    <p className='pt-3'>These low-profile sneakers are your perfect casual wear companion. Featuring a 
+                    <p className='pt-2 pt-md-3'>These low-profile sneakers are your perfect casual wear companion. Featuring a 
                     durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
 
-                    <div className='d-flex d-lg-block justify-content-between align-items-center mb-2 mb-lg-0 w-100'>
-                        <div className='d-flex gap-2 align-items-center mb-0 mb-lg-2'>
+                    <div className='d-flex d-md-block justify-content-between align-items-center mb-2 mb-md-0 w-100'>
+                        <div className='d-flex gap-2 align-items-center mb-0 mb-md-2'>
                             <p className='text-black fw-bold fs-4 m-0'>$125.00</p>
                             <p className='bg-black text-white fw-bold rounded-3 py-0 px-2 m-0'>50%</p>
                         </div>
-                        <p className='text-decoration-line-through fw-bold mb-0 mb-lg-4'>$250.00</p>
+                        <p className='text-decoration-line-through fw-bold mb-0 mb-md-4'>$250.00</p>
                     </div>
 
-                    <div className='d-flex gap-3'>
-                        <div className='d-flex align-items-center gap-3' style={{backgroundColor: "#f7f8fd"}}>
+                    <div className='div-buy-info d-block d-md-flex gap-3'>
+                        <div className='d-flex align-items-center gap-auto justify-content-between gap-md-3 rounded-3' style={{backgroundColor: "#f7f8fd"}}>
                             <button className="btn-change-count" onClick={() => setCountItems(prev => prev === 0 ? 0 : prev - 1)}><img src="images/icon-minus.svg" alt="icon-minus"></img></button>
                             <p className='m-0 fw-bold text-black'>{countItems}</p>
                             <button className="btn-change-count" onClick={() => setCountItems(prev => prev + 1)}><img src="images/icon-plus.svg" alt="icon-plus"></img></button>
                         </div>
 
-                        <button className='btn-add-to-cart' onClick={() => {countItems !== 0 ? setAddToCart(true) : setAddToCart(false)}}>
+                        <button className='btn-add-to-cart mt-3 mt-md-0' onClick={() => {countItems !== 0 ? setAddToCart(true) : setAddToCart(false)}}>
                             <i className="bi bi-cart2 pe-2"></i>
                             Add to cart
                         </button>
@@ -56,7 +60,7 @@ function Product({countItems, setCountItems, setAddToCart}: Props) {
                 </div>
             </div>
 
-            {isModalShowed === true ?
+            {(isModalShowed === true && window.innerWidth > 768) ?
                 <>
                     <div className="modal-backdrop fade show"></div>
                     
